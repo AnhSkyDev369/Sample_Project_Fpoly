b5709b203244425447818ecf634b1aaea080c469
Anh Sky Dev <anhhh11345@gmail.com>
2024-04-05 23:07:07 +0700

    Add files .gitignore

--- commons/env.example.php
+++ commons/env.example.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // Khai báo các biến môi trường dùng Global
 define('PATH_CONTROLLER',   __DIR__ . '/../controllers/');
@@ -23,4 +23,23 @@ define('DB_NAME',       'mvc_basic');
 define('STATUS_DRAFT',      'draft');
 define('STATUS_PUBLISHED',  'published');
 
+
+//Khai báo các hàm, biến môi trường dùng Global
+// Đây là cú pháp khai báo hằng
+//Viết in hoa và phân cách nhau giữa các từ có nghĩa = dấu gạch dưới
+//__DIR__ là magic function của PHP, có ý nghĩa khi sử dụng sẽ lấy ra đường dẫn đến thưu mục chưa file hiện tại.
+// Sử dụng dấu . trong  define('PATH_CONTROLLER', __DIR__ . './controllers'); có tác dụng giúp file env lùi ra 1 cấp khỏi folders commons và cùng cấp với controllers
+// define('PATH_CONTROLLER',       __DIR__ . '/../controllers/');
+// define('PATH_MODEL',            __DIR__ . '/../models/');
+// define('PATH_VIEW',             __DIR__ . '/../views/');
+
+// define('BASE_URL', 'http://localhost/mvc-basic/');
+
+//C:\laragon\www\MVC_BASIC\commons/../controllers ~ C:\laragon\www\MVC_BASIC\commons
+
+// define('DB_HOST',       'localhost');
+// define('DB_PORT',       ' 3306');
+// define('DB_USERNAME',   'root');
+// define('DB_PASSWORD' ,  '');
+// define('DB_NAME',       'MVC_BASIC');
 
